f069f41b42d75a8208778de703bf035e614cad85
kaolinhart <kaolinhart@kaolins-mbp>
2015-12-09 20:18:17 -0500

    Added Init method. Creates arrays for movement, and animation. Created Player Class Added userInput in the Player Class

--- God of Fire/src/Entity.java
+++ God of Fire/src/Entity.java
@@ -1,6 +1,10 @@
 import java.awt.*;
+]
 import java.awt.image.BufferedImage;
-
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tests.xml.Inventory;
 
 public abstract class Entity {
@@ -8,62 +12,92 @@ public abstract class Entity {
 	/**
 	 * Location of the player
 	 */
-	private Point playerLocation;
+	protected Point playerLocation;
 	
 	/**
 	 * Players inventory
 	 */
-	private Inventory inventory;
+	protected Inventory inventory;
+	
+	/**
+	 * Spritesheet for all images
+	 */
+	protected SpriteSheet spriteSheet;
+	
+	/**
+	 * Orientation of the character
+	 * @author kaolinhart
+	 *
+	 */
+	protected enum direction {up, down, left, right};
+	
+	protected direction orientation;
 	
 	/**
 	 * Array of images for multidirectional movement
 	 */
-	private BufferedImage[] movementImages;
+	protected Image[] upwardsMovementImages, downwardMovementImages, rightMovementImages, leftMovementImages;
+
+	/**
+	 * What animation is currently being used
+	 */
+	protected Animation currentAnimation;
 	
 	/**
-	 * Array of images for when entities not moving
+	 * Animations for the entity
 	 */
-	private BufferedImage[] stationaryImages;
+	protected Animation upwardsMovementAnimation, downwardMovementAnimation, leftMovementAnimation, rightMovementAnimation  ;
+	
 	
 	/**
-	 * Array of images for entities physical attacks
+	 * Array of images and animation for when entities not moving
 	 */
-	private BufferedImage[] attackingImages;
+	protected Image[] stationaryImages;
+	
+	protected Animation stationaryAnimation;
 	
 	/**
-	 * Array of images for entities casting
+	 * Array of images and animation for entities physical attacks
 	 */
-	private BufferedImage[] castingImages;
+	protected Image[] attackingImages, castingImages;
 	
+	protected Animation attackingAnimation, castingAnimation;
+	
+
 	/**
 	 * If entities alive or not
 	 */
-	private boolean isDead;
+	protected boolean isDead;
 	
 	/**
 	 * The entities current health
 	 */
-	private int health;
+	protected int health;
+	
+	/**
+	 * How fast the entity moves
+	 */
+	protected int movementSpeed;
 	
 	/**
 	 * The entities maximum health
 	 */
-	private int maximumHealth;
+	protected int maximumHealth;
 	
 	/**
 	 * Damage capable by player. Increase
 	 */
-	private int attack;
+	protected int attack;
 	
 	/**
 	 * Defense modifier. Reduces damage taken
 	 */
-	private int defense;
+	protected int defense;
 	
 	/**
 	 * Modifier for critical chance.
 	 */
-	private int critChance;
+	protected int critChance;
 	
 	/**
 	 * Default constructor
@@ -79,9 +113,40 @@ public abstract class Entity {
 		this.defense=defense;
 	}
 	
+	
+	/**
+	 * Looks like a clusterfuck now, but it will improve!
+	 * @throws SlickException
+	 */
+	public void init() throws SlickException{
+		upwardsMovementImages= {spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		downwardMovementImages={spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		rightMovementImages={spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		leftMovementImages={spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		stationaryImages= {spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		attackingImages =  {spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		castingImages= {spriteSheet.getSubImage(x, y, width, height), spriteSheet.getSubImage(x, y, width, height)};
+		
+		upwardsMovementAnimation=new Animation(upwardsMovementImages, 1, false);
+		downwardMovementAnimation=new Animation(downwardMovementImages, 1, false);
+		rightMovementAnimation=new Animation(rightMovementImages, 1, false);
+		leftMovementAnimation=new Animation(leftMovementImages, 1, false);
+		stationaryAnimation=new Animation(stationaryImages, 1, false);
+		attackingAnimation=new Animation(attackingImages, 1, false);
+		castingAnimation=new Animation(castingImages, 1, false);
+		
+	}
+	
 	public boolean isColliding(Entity e){
 		return isDead;
-		
+	}
+	
+	/**
+	 * Sets orientation of the entity
+	 * @param d
+	 */
+	protected void setOrientation(direction d){
+		orientation=d;
 	}
 	
 	/**
