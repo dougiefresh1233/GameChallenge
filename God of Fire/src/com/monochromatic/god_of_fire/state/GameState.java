a9fb114eae5650a31d02c71696ab80318a535181
dougiefresh1233 <douglas.cannon18@gmail.com>
2015-12-13 11:55:38 -0600

    Moved spawn to better test location

--- God of Fire/src/com/monochromatic/god_of_fire/state/GameState.java
+++ God of Fire/src/com/monochromatic/god_of_fire/state/GameState.java
@@ -1,5 +1,4 @@
 package com.monochromatic.god_of_fire.state;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -9,13 +8,15 @@ import org.newdawn.slick.tiled.TiledMap;
 
 import com.monochromatic.god_of_fire.SetupClass;
 import com.monochromatic.god_of_fire.entity.living.Player;
+import com.monochromatic.god_of_fire.entity.living.monster.Monster;
 import com.monochromatic.god_of_fire.graphics.Camera;
 
 public class GameState extends BasicGameState{
 		 
 	//declare variables here
-	TiledMap testMap;
+	TiledMap map;
 	Player player;
+	Monster monster;
 	
 	private final int gameWidth=640;
 	private final int gameHeight=480;
@@ -24,16 +25,18 @@ public class GameState extends BasicGameState{
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		 //All variables defined here
-		testMap=new TiledMap("resources/Map.tmx");//TODO Replace with real map
-		player=new Player(this, 150, 150, 10, 10, 10,5,testMap);
+		map=new TiledMap("resources/Map.tmx");//TODO Replace with real map
+		player=new Player(this, 800, 1020, 10, 10, 10,5,map);
+		//monster = new Monster(map, 150, 150, 200, 10, 20, 3); 
 		playersPerspective=new Camera(this, 0,0);
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		//Draw objects on screen
-		testMap.render(0 - (int)playersPerspective.getxOffset(), 
+		map.render(0 - (int)playersPerspective.getxOffset(), 
 				0- (int)playersPerspective.getyOffset());
 		player.render();
+		//monster.render();
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
