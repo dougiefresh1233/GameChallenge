16c05d8578289fad72858eb7433cb4fe2df8af8c
K. Levi Hart <kaolin.l.hart@gmail.com>
2015-12-14 18:52:06 -0500

    Some magical stuff in the player class

--- God of Fire/src/com/monochromatic/god_of_fire/entity/living/Player.java
+++ God of Fire/src/com/monochromatic/god_of_fire/entity/living/Player.java
@@ -25,21 +25,21 @@ public class Player extends LivingEntity {
 	/**
 	 * Players inventory
 	 */
-	
+
 	protected Inventory inventory;
-	
+
 	Point cameraOffsetPoint=new Point(0,0);
-	
-	
+
+
 	ParticleSystem particleSystem;
 	Image particleImage;
 	ConfigurableEmitter emitter;
 
-	
+
 	MeleeWeapon equippedWeapon;
 
 	private boolean superPower;
-	
+
 	public Player(GameState g, int x, int y, int h, int a, int d, int c) {
 		super(g, x, y, h, a, d, c);
 		movementSpeed=2;
@@ -52,12 +52,12 @@ public class Player extends LivingEntity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		equippedWeapon=new MeleeWeapon("Sword", "A sword", "resources/shittysword.png",
-				 c, true, true, 10, 10, 10);
+				c, true, true, 10, 10, 10);
 		equippedWeapon.equip(cameraOffsetPoint);
 	}
-	
+
 	public void initParticles(){
 
 		try {
@@ -77,7 +77,7 @@ public class Player extends LivingEntity {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	public void update(GameContainer gameScreen){
 		particleSystem.update(10);
@@ -89,52 +89,76 @@ public class Player extends LivingEntity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		equippedWeapon.update();
 		gameState.getCamera().centerCamera(this);
 		cameraOffsetPoint.setLocation((int)(location().getX()-gameState.getCamera().getxOffset()),
-					(int)(location().getY()-gameState.getCamera().getyOffset()));
-		
-	
+				(int)(location().getY()-gameState.getCamera().getyOffset()));
+
+
 	}
-	
+
 	private void userInput(GameContainer gameScreen) throws SlickException{
 		Input userInput = gameScreen.getInput();
 		if(userInput.isKeyDown(Input.KEY_W))
 			move(Direction.UP);
 		else
 			upwardsMovementAnimation.stop();
-		
+
 		if(userInput.isKeyDown(Input.KEY_S))
 			move(Direction.DOWN);
 		else
 			downwardMovementAnimation.stop();
-		
+
 		if (userInput.isKeyDown(Input.KEY_A))
 			move(Direction.LEFT);
 		else
 			leftMovementAnimation.stop();
-		
+
 		if (userInput.isKeyDown(Input.KEY_D))
 			move(Direction.RIGHT);
 		else
 			rightMovementAnimation.stop();
-		
-		if (userInput.isKeyDown(Input.KEY_F))
+
+
+		if (userInput.isKeyDown(Input.KEY_F)){
 			superPower=true;
-		else
+			switch(orientation){
+			case UP: emitter.angularOffset.setValue(0); 
+			break;
+			case DOWN: emitter.angularOffset.setValue(180);
+			break;
+			case LEFT: emitter.angularOffset.setValue(270);
+			break;
+			case RIGHT: emitter.angularOffset.setValue(90);
+			break;
+			}
+			
+			particleSystem.render();
+			particleSystem.setVisible(true);
+			superPower=true;
+			
+			List<Monster> targets = EntityUtility.intersectsMonsters(getGameState().getEC().getEntities(), 
+					calculateAttackArea(orientation));
+			for (Monster m : targets)
+				if(m.adjustHealth(calculateAttack()))
+					m.kill();
+		}else{
+			particleSystem.reset();
 			superPower=false;
-		
+		}
+
+
 		if(userInput.isKeyPressed(Input.KEY_SPACE)){
 			switch (orientation) {
-				case UP: equippedWeapon.attack(180, 1); 
-					break;
-				case DOWN: equippedWeapon.attack(180, -1);
-					break;
-				case LEFT: equippedWeapon.attack(270, -1);
-					break;
-				case RIGHT: equippedWeapon.attack(270, 1);
-					break;
+			case UP: equippedWeapon.attack(180, 1); 
+			break;
+			case DOWN: equippedWeapon.attack(180, -1);
+			break;
+			case LEFT: equippedWeapon.attack(270, -1);
+			break;
+			case RIGHT: equippedWeapon.attack(270, 1);
+			break;
 			}
 			List<Monster> targets = EntityUtility.intersectsMonsters(getGameState().getEC().getEntities(), 
 					calculateAttackArea(orientation));
@@ -143,34 +167,34 @@ public class Player extends LivingEntity {
 					m.kill();
 		}
 	}
-	
+
 	private double[] calculateAttackArea(Direction d) {
 		double[] area = new double[4];
 		switch (orientation) {
-			case UP: 
-				area[0] = location().getX() - 48;
-				area[1] = location().getY() + 16;
-				area[2] = location().getX() + 48;
-				area[3] = location().getY() + 48;
-				break;
-			case DOWN: 
-				area[0] = location().getX() - 48;
-				area[1] = location().getY() - 48;
-				area[2] = location().getX() + 48;
-				area[3] = location().getY() - 16;
-				break;
-			case LEFT:
-				area[0] = location().getX() - 48;
-				area[1] = location().getY() - 48;
-				area[2] = location().getX() - 16;
-				area[3] = location().getY() + 48;
-				break;
-			default:
-				area[0] = location().getX() + 16;
-				area[1] = location().getY() - 48;
-				area[2] = location().getX() + 48;
-				area[3] = location().getY() + 48;
-				break;
+		case UP: 
+			area[0] = location().getX() - 48;
+			area[1] = location().getY() + 16;
+			area[2] = location().getX() + 48;
+			area[3] = location().getY() + 48;
+			break;
+		case DOWN: 
+			area[0] = location().getX() - 48;
+			area[1] = location().getY() - 48;
+			area[2] = location().getX() + 48;
+			area[3] = location().getY() - 16;
+			break;
+		case LEFT:
+			area[0] = location().getX() - 48;
+			area[1] = location().getY() - 48;
+			area[2] = location().getX() - 16;
+			area[3] = location().getY() + 48;
+			break;
+		default:
+			area[0] = location().getX() + 16;
+			area[1] = location().getY() - 48;
+			area[2] = location().getX() + 48;
+			area[3] = location().getY() + 48;
+			break;
 		}
 		return area;
 	}
@@ -178,29 +202,29 @@ public class Player extends LivingEntity {
 	@Override
 	public void attackAnim() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void hurtAnim() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
 	@Override
 	public void healedAnim() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
 	@Override
 	public void deathAnim() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public Inventory getInventory() {
 		return inventory;
 	}
@@ -208,7 +232,7 @@ public class Player extends LivingEntity {
 	public void setInventory(Inventory inventory) {
 		this.inventory = inventory;
 	}
-	
+
 	@Override
 	public void render() {
 		if(initComplete){
@@ -216,20 +240,20 @@ public class Player extends LivingEntity {
 					(int)(location().getY()-gameState.getCamera().getyOffset()));
 			equippedWeapon.render();
 		}
-		
+
 		particleSystem.render();
 		if(superPower){
 			particleSystem.setVisible(true);
 		}else
 			particleSystem.setVisible(false);
-		
-	
+
+
 	}
 
 
 	@Override
 	public void collide(Entity e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
---
+++ God of Fire/src/com/monochromatic/god_of_fire/entity/nonliving/EntityMagicBall.java
@@ -0,0 +1,14 @@
+package com.monochromatic.god_of_fire.entity.nonliving;
+
+import com.monochromatic.god_of_fire.enums.DamageTarget;
+import com.monochromatic.god_of_fire.enums.Direction;
+import com.monochromatic.god_of_fire.state.GameState;
+
+public class EntityMagicBall extends EntityProjectile{
+
+	public EntityMagicBall(GameState g, int x, int y, Direction d, int s, int a) {
+		super(g, x, y, d, s, a);
+		// TODO Auto-generated constructor stub
+	}
+
+}
